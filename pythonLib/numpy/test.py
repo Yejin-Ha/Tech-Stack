eb45212df2517131267c225b195259ea81f6c791
hyj <hyj1004mu@naver.com>
2021-08-09 17:44:42 +0900

    0809 수업 정리

---
+++ pythonLib/numpy/test.py
@@ -0,0 +1,12 @@
+number = "31415926"
+dictionary = ["may","i","have","a","large","container","of","coffee"]
+
+def solution(number, dictionary):
+    dictionary.sort()
+
+    for i in number:
+        print(i)
+            
+        
+
+solution(number, dictionary)
